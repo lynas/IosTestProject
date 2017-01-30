1af7df6450aac7107f88e79934a6781d20c264ab
sazzad <sazzad.islam@dsinnovators.com>
2017-01-30 17:58:27 +0600

    create album done

--- IosTestProject/ViewController.swift
+++ IosTestProject/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let albumName = "SocialCam"
+    let pservice = PhotoService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func createButton(_ sender: UIButton) {
+        pservice.createAlbum(name: albumName, completion: {
+            ph in
+            
+        })
+    }
+    
+    @IBAction func getButton(_ sender: UIButton) {
+        pservice.getAlbum(name: albumName, completion: {
+            album in
+            print(album?.estimatedAssetCount ?? 0)
+        })
+    }
+    
+    
+    
 }
 
